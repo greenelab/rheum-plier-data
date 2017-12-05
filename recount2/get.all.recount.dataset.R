7f69dd171b3a5e18d75f73a7930eb981c9f57feb
Qiwen Hu <huqiwen0313@gmail.com>
2017-12-05 14:23:55 -0500

    Format of code modified
    Add back deleted lines for downloading recount samples in get.all.recount.dataset.R

--- recount2/get.all.recount.dataset.R
+++ recount2/get.all.recount.dataset.R
@@ -1,36 +1,64 @@
+# Qiwen Hu - 2017
+# Processing all recounts datasets
+# genes expression are normalized by RPKM
+# It can be run from the command line using Rscript get.all.recount.dataset.R
+#
+# Output: 
+# Normalized gene expression for each sample
+
 library(recount)
 
-#get RPKM value for each gene - function modified from recount package
+# Get RPKM value for each gene
 getRPKM <- function(rse, length_var = 'bp_length', mapped_var = NULL) { 
+  # Computes the RPKM value for each gene in the sample.
+  #
+  # Args: 
+  #  rse: A RangedSummarizedExperiment-class object in recount package
+  #  length_var: A length 1 character vector with the column name from rowData(rse) that has
+  #              the coding length. For gene level objects from recount this is bp_length. If
+  #              NULL, then it will use width(rowRanges(rse)) which should be used for exon RSEs.
+  #  mapped_var: A length 1 character vector with the column name from colData(rse) that has
+  #              the number of reads mapped. If NULL (default) then it will use the column 
+  #              sums of the counts matrix
+  # Returns:
+  #   RPKM value for each sample
   mapped <- if(!is.null(mapped_var)) colData(rse)[, mapped_var] 
 		else colSums(assays(rse)$counts) 
   bg <- matrix(mapped, ncol = ncol(rse), nrow = nrow(rse), byrow = TRUE) 
   len <- if(!is.null(length_var)) rowData(rse)[, length_var] 
 		else width(rowRanges(rse)) 
   wid <- matrix(len, nrow = nrow(rse), ncol = ncol(rse), byrow = FALSE) 
-  assays(rse)$counts / (wid/1000) / (bg/1e6) 
+  rpkm <- assays(rse)$counts / (wid/1000) / (bg/1e6) 
+  return(rpkm)
 } 
 
+input.dir <- file.path("recount2", "data")
 
-dir <- '/home/qhu/Recount'
-
-##get all samples from recount database
+# Get all samples from recount database
 metasample.sra <- all_metadata(subset = "sra", verbose = TRUE)
 metasample.sra <- as.data.frame(metasample.sra)
+
+# Remove samples without description
 metadata.nonempty <- metasample.sra[!is.na(metasample.sra$characteristics), ] 
-sample_list <- unique(metadata.nonempty$project)
+included.sample.list <- unique(metadata.nonempty$project)
+
+# Download all recount2 samples in included.sample.list
+for(i in 1:length(included.sample.list)) {
+      url2 <- download_study(included.sample.list[i], type = c("rse-gene"),
+                             outdir = paste(input.dir, sample_list[i], sep = ""))
+}
 
 rpkm <- data.frame()
 
-for(i in 1:length(sample_list)) {
-  rse_gene <- get(load(file.path(dir, sample_list[i], 'rse_gene.Rdata')))
+for(i in 1:length(included.sample.list)) {
+  rse_gene <- get(load(file.path(input.dir, included.sample.list[i], 'rse_gene.Rdata')))
   if(i == 1) {
     rpkm <- as.data.frame(getRPKM(rse_gene))
     rpkm$id <- rownames(rpkm)
-    } else {
+  } else {
     rpkm.tmp <- as.data.frame(getRPKM(rse_gene))
     rpkm.tmp$id <- rownames(rpkm.tmp)
     rpkm <- merge(rpkm, rpkm.tmp, by=c("id"))
-    }
+  }
 }
-save(rpkm,file = "recount.rpkm.RData")
+save(rpkm, file = "recount.rpkm.RData")
--- recount2/recount.plier.R
+++ recount2/recount.plier.R
@@ -1,3 +1,11 @@
+# Qiwen Hu - 2017
+# PLIER model for recount datasets
+# Run PLIER for all recount datasets, genes are normalized by RPKM
+# It can be run from the command line using Rscript recount.plier.R
+#
+# Output: 
+# PLIER model for all the samples
+
 library(recount)
 library(PLIER)
 library('biomaRt')
@@ -6,31 +14,33 @@ data(bloodCellMarkersIRISDMAP)
 data(svmMarkers)
 data(canonicalPathways)
 load("recount.rpkm.RData")
+set.seed(12345)
 
-#transform ensemble id to genesymbol
-mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+# Transform ensemble id to genesymbol
+mart <- biomaRt::useDataset("hsapiens_gene_ensembl", biomaRt::useMart("ensembl"))
 genes <- unlist(lapply(strsplit(rpkm$id, "[.]"), `[[`, 1))
 rpkm$ensembl_gene_id <- unlist(lapply(strsplit(rpkm$id, "[.]"), `[[`, 1))
-G_list <- getBM(filters= "ensembl_gene_id",
+gene.list <- getBM(filters = "ensembl_gene_id",
 		attributes= c("ensembl_gene_id", "hgnc_symbol"),
-		values=genes, mart= mart)
-G_list <- G_list[G_list$hgnc_symbol != "",]
+		values = genes, mart = mart)
+gene.list <- G_list[G_list$hgnc_symbol != "", ]
 
-rpkm <- merge(rpkm, G_list, by=c("ensembl_gene_id"))
-rownames(rpkm) <- make.names(rpkm$hgnc_symbol, unique=TRUE)
+rpkm <- merge(rpkm, G_list, by = c("ensembl_gene_id"))
+rownames(rpkm) <- make.names(rpkm$hgnc_symbol, unique = TRUE)
 
-#remove redundant information
-rpkm <- rpkm[, -1*ncol(rpkm)]
-rpkm <- rpkm[, -1*c(1,2)]
+# Remove redundant hgnc_symbol here
+rpkm <- rpkm[, -ncol(rpkm)]
 
-#run PLIER###
-allPaths=combinePaths(bloodCellMarkersIRISDMAP, svmMarkers,canonicalPathways)
-cm.genes=commonRows(allPaths, rpkm)
+# Remove redundant ensembl_gene_id and gene_id
+rpkm <- rpkm[, -1*c(1, 2)]
+
+# Run PLIER
+allPaths <- combinePaths(bloodCellMarkersIRISDMAP, svmMarkers,canonicalPathways)
+cm.genes <- commonRows(allPaths, rpkm)
 rpkm <- rowNorm(rpkm)
 k <- num.pc(rpkm[cm.genes, ])
 
 rpkm <- as.matrix(as.data.frame(rpkm))
 plierResult <- PLIER(rpkm[cm.genes, ], allPaths[cm.genes, ],
-		     k=round((k + k*0.3),0), trace=T)
+		     k = round((k + k*0.3), 0), trace = TRUE)
 save(plierResult, file = "recount.all.data.plier.RData")
-
