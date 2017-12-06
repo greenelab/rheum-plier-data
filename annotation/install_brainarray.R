ae3c1c8ea784ad877766806ad2369194e8334d8f
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2017-12-06 13:45:19 -0500

    Fix: spacing

--- annotation/install_brainarray.R
+++ annotation/install_brainarray.R
@@ -27,9 +27,9 @@ InstallBrainarray <- function(platform, org.code, ba.version) {
 
 	# probe version for use with SCAN.UPC
 	probe.pkg.name <- paste0(platform, org.code, "entrezgprobe_",
-						      	       ba.version, ".tar.gz")
+                         ba.version, ".tar.gz")
 	probe.url <- paste0("http://mbni.org/customcdf/", ba.version, 
-											"/entrezg.download/", probe.pkg.name)
+	                    "/entrezg.download/", probe.pkg.name)
 	devtools::install_url(probe.url)
 
 	# cdf version for use with affy::RMA
@@ -45,23 +45,23 @@ InstallBrainarray <- function(platform, org.code, ba.version) {
 
 # HGU133Plus2
 InstallBrainarray(platform = "hgu133plus2",
-									org.code = "hs",
-									ba.version = "21.0.0")
+                  org.code = "hs",
+                  ba.version = "21.0.0")
 
 # HGU133A
 InstallBrainarray(platform = "hgu133a",
-									org.code = "hs",
-									ba.version = "21.0.0")
+                  org.code = "hs",
+                  ba.version = "21.0.0")
 
 # HGU133B
 InstallBrainarray(platform = "hgu133b",
-									org.code = "hs",
-									ba.version = "21.0.0")
+                  org.code = "hs",
+                  ba.version = "21.0.0")
 
 # HGU95Av2
 InstallBrainarray(platform = "hgu95av2",
-									org.code = "hs",
-									ba.version = "21.0.0")
+                  org.code = "hs",
+                  ba.version = "21.0.0")
 
 # hugene11st
 InstallBrainarray(platform = "hugene11st",
