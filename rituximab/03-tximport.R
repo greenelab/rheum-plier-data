a1abc7c92ab0749e2f681c6a2668c7ccbb17a77a
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2018-06-19 09:17:03 -0400

    Add initial processing code for RNA-seq pipeline (#23)
    * Ignore files related to RNA-seq
    
    * Add salmon index and quant shell scripts
    
    * Add tximport and associated Dockerfile
    
    * Ignore tximport output
    
    * Update: improve docs

---
+++ rituximab/03-tximport.R
@@ -0,0 +1,30 @@
+# J. Taroni 2018
+# Summarize data to the gene-level with tximport
+
+library(ensembldb)
+
+StripTrailing <- function(filename) {
+  # in order to get the tx2gene data.frame ENST ids to match, strip ".1", etc.
+  # takes a filename (including path) output from salmon (quant.sf)
+  # reads in the quant file as a tsv and strips the ENST version info
+  # writing the altered data.frame to a tsv with the input filename
+  df <- readr::read_tsv(filename)
+  df$Name <- sub("\\..*", "", df$Name)
+  readr::write_tsv(df, path = filename)
+}
+
+# derive transcript to gene mapping from gtf
+ensembldb::ensDbFromGtf("Homo_sapiens.GRCh38.92.gtf")
+edb <- ensembldb::EnsDb("Homo_sapiens.GRCh38.92.sqlite")
+tx <- ensembldb::transcriptsBy(edb, by = "gene")
+tx.df <- as.data.frame(tx@unlistData)
+tx2gene <- tx.df[, c("tx_name", "gene_id")]
+
+# vector of quant.sf files
+sf.files <- list.files("quants", recursive = TRUE, full.names = TRUE,
+                       pattern = "quant.sf")
+sapply(sf.files, StripTrailing)
+
+# main tximport function
+txi <- tximport::tximport(sf.files, type = "salmon", tx2gene = tx2gene)
+saveRDS(txi, file = "tximport.RDS")
