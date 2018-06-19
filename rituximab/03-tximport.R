46b61f08e58374a6bf82cda338932e9ca695f0e4
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2018-06-19 09:10:32 -0400

    Update: improve docs

--- rituximab/03-tximport.R
+++ rituximab/03-tximport.R
@@ -5,6 +5,9 @@ library(ensembldb)
 
 StripTrailing <- function(filename) {
   # in order to get the tx2gene data.frame ENST ids to match, strip ".1", etc.
+  # takes a filename (including path) output from salmon (quant.sf)
+  # reads in the quant file as a tsv and strips the ENST version info
+  # writing the altered data.frame to a tsv with the input filename
   df <- readr::read_tsv(filename)
   df$Name <- sub("\\..*", "", df$Name)
   readr::write_tsv(df, path = filename)
